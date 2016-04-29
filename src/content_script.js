7108b46f4b0a87d238f8494e8bd37da3667931d3
Pornvibool Tippayawat <pornvibool.t@thomsonreuters.com>
2016-04-29 16:05:58 +0700

    draft

--- src/content_script.js
+++ src/content_script.js
@@ -7,52 +7,54 @@ function inject(evt) {
     var observer = new MutationObserver(function (mutations) {
 
         var elem = document.getElementsByClassName("prt-head-current");
-        if (elem.length == 0 || elem[0].textContent != 'Bookmaker')
+        if (elem.length == 0 ||
+            (elem[0].textContent != 'Bookmaker' && elem[0].textContent != 'Place a Bet'))
             return;
 
         var areaList = document.querySelectorAll(".lis-area .point");
-        if (areaList.length > 0) {
-            console.log("found point!");
-            // N W E S
-            console.log(
-                areaList[0].textContent + ',' +
-                areaList[1].textContent + ',' +
-                areaList[2].textContent + ',' +
-                areaList[3].textContent);
+        if (areaList.length == 0)
+            return;
+
+        console.log("found point!");
+        // N W E S
+        console.log(
+            areaList[0].textContent + ',' +
+            areaList[1].textContent + ',' +
+            areaList[2].textContent + ',' +
+            areaList[3].textContent);
             
-            // Save it using the Chrome extension storage API.
-            var record = {
-                time: new Date().toLocaleTimeString(),
-                n: areaList[0].textContent,
-                w: areaList[1].textContent,
-                e: areaList[2].textContent,
-                s: areaList[3].textContent,
-            };
+        // Save it using the Chrome extension storage API.
+        var record = {
+            time: new Date().toLocaleTimeString(),
+            n: areaList[0].textContent,
+            w: areaList[1].textContent,
+            e: areaList[2].textContent,
+            s: areaList[3].textContent,
+        };
 
-            chrome.storage.local.get('bookmaker', function (obj) {
-                if (obj.hasOwnProperty('bookmaker')) {
+        chrome.storage.local.get('bookmaker', function (obj) {
+            if (obj.hasOwnProperty('bookmaker')) {
 
-                    var bookmaker = obj.bookmaker;
-                    //get last record
-                    var latestRecord = bookmaker[bookmaker.length - 1];
-                    if (latestRecord.n == record.n &&
-                        latestRecord.w == record.w &&
-                        latestRecord.e == record.e &&
-                        latestRecord.s == record.s)
-                        return; // same record, do nothing
+                var bookmaker = obj.bookmaker;
+                //get last record
+                var latestRecord = bookmaker[bookmaker.length - 1];
+                if (latestRecord.n == record.n &&
+                    latestRecord.w == record.w &&
+                    latestRecord.e == record.e &&
+                    latestRecord.s == record.s)
+                    return; // same record, do nothing
                     
-                    bookmaker.push(record)
-                    chrome.storage.local.set(obj, function () {
-                        console.log('record added');
-                    });
-                }
-                else {
-                    chrome.storage.local.set({ 'bookmaker': [record] }, function () {
-                        console.log('record added');
-                    });
-                }
-            });
-        }
+                bookmaker.push(record)
+                chrome.storage.local.set(obj, function () {
+                    console.log('record added');
+                });
+            }
+            else {
+                chrome.storage.local.set({ 'bookmaker': [record] }, function () {
+                    console.log('record added');
+                });
+            }
+        });
     });
 
     // configuration of the observer:
